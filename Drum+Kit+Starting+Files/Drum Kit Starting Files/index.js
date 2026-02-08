11431f1eb7e6d18926eaf6e9cbde54fa980bcb71
Aishwarya <124514705+GOver1519@users.noreply.github.com>
2026-02-08 15:26:09 +0530

    Add files via upload

---
+++ Drum+Kit+Starting+Files/Drum Kit Starting Files/index.js
@@ -0,0 +1,40 @@
+var drumButtons = document.querySelectorAll(".drum").length;
+
+for(var i = 0; i<drumButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        
+    var buttonInnerHTML = this.innerHTML;
+    console.log(buttonInnerHTML);
+    switch(buttonInnerHTML){
+        case "w":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "a":
+            var kickBass = new Audio("sounds/kick-bass.mp3");
+            kickBass.play();
+            break;
+        case "s":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "d":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        case "j":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "k":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "l":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        default:
+    }
+    })
+}
